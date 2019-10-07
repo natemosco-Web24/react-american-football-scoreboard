a87c9cd2e31d091e7ccbef6a641ec6092d8705bb
Nathaniel Mosco <natemosco@gmail.com>
2019-10-07 16:02:12 -0400

    added prompt to set team and score manually

--- src/App.js
+++ src/App.js
@@ -7,13 +7,26 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [home, setHome] = useState(0)
   const [away, setAway] = useState(0)
+  const [homeTeam, setHomeTeam] = useState("Home")
+  const [awayTeam, setAwayTeam] = useState("Away")
+  const setup = () => {
+    let homeName = prompt("Home Team Name?", homeTeam)
+    let homeScore = prompt("What is the Home Team Score?", home)
+    let awayName = prompt("Away Team Name?", awayTeam)
+    let awayScore = prompt("What is the Away Team Score?", away)
 
+    if (awayTeam === "Away" && homeTeam === "Home") {
+      alert(setHomeTeam(homeName) + setHome(homeScore) + setAwayTeam(awayName) + setAway(awayScore))
+    } else {
+      alert(setHome(homeScore) + setAway(awayScore))
+    }
+  }
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
+          <div className="home" onClick={() => { setup() }}>
+            <h2 className="home__name">{homeTeam}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -21,7 +34,7 @@ function App() {
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">{awayTeam}</h2>
             <div className="away__score">{away}</div>
           </div>
         </div>
